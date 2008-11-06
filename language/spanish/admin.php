0fdbea109621c0825d7fe2459c8359d164eb97dd
Juan Calzado <debianus@impresscms.org>
2008-11-06 21:37:41 +0000

    Adding Spanish translation for iforum
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/iforum/trunk@6495 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/spanish/admin.php
@@ -0,0 +1,275 @@
+<?php  //* Spanish Translation by JulioNC  *// 
+// $Id: admin.php,v 1.3 2005/10/19 17:20:33 phppp Exp $
+//%%%%%%	File Name  index.php   	%%%%%
+define("_AM_NEWBB_FORUMCONF","Configuración del foro");
+define("_AM_NEWBB_ADDAFORUM","Agregar foro");
+define("_AM_NEWBB_SYNCFORUM","Sincronizar foros/temas");
+define("_AM_NEWBB_REORDERFORUM","Ordenar foros");
+define("_AM_NEWBB_FORUM_MANAGER","Administrar foro");
+define("_AM_NEWBB_PRUNE_TITLE","Sistema de purgado de foros");
+define("_AM_NEWBB_CATADMIN","Administrar categoría");
+define("_AM_NEWBB_GENERALSET", "Configuración del módulo" );
+define("_AM_NEWBB_MODULEADMIN","Administrar módulo:");
+define("_AM_NEWBB_HELP","Ayuda");
+define("_AM_NEWBB_ABOUT","Créditos");
+define("_AM_NEWBB_BOARDSUMMARY","Estadísticas foro");
+define("_AM_NEWBB_PENDING_POSTS_FOR_AUTH","Mensaje pendiente de autorización");
+define("_AM_NEWBB_POSTID","ID Mensaje");
+define("_AM_NEWBB_POSTDATE","Fecha del mensaje");
+define("_AM_NEWBB_POSTER","Enviado por");
+define("_AM_NEWBB_TOPICS","Asunto");
+define("_AM_NEWBB_SHORTSUMMARY","Resumen foro");
+define("_AM_NEWBB_TOTALPOSTS","Total mensajes");
+define("_AM_NEWBB_TOTALTOPICS","Total temas");
+define("_AM_NEWBB_TOTALVIEWS","Total visitas");
+define("_AM_NEWBB_BLOCKS","Bloques");
+define("_AM_NEWBB_SUBJECT","Asunto");
+define("_AM_NEWBB_APPROVE","Aprobar mensaje");
+define("_AM_NEWBB_APPROVETEXT","Contenido de este mensaje");
+define("_AM_NEWBB_POSTAPPROVED","El mensaje ha sido aprobado");
+define("_AM_NEWBB_POSTNOTAPPROVED","El mensaje no ha sido aprobado");
+define("_AM_NEWBB_POSTSAVED","El mensaje ha sido guardado");
+define("_AM_NEWBB_POSTNOTSAVED","El mensaje no ha sido guardado");
+
+define("_AM_NEWBB_TOPICAPPROVED","El mensaje ha sido aprobado");
+define("_AM_NEWBB_TOPICNOTAPPROVED","El mensaje NO ha sido aprobado");
+define("_AM_NEWBB_TOPICID","ID del mensaje");
+define("_AM_NEWBB_ORPHAN_TOPICS_FOR_AUTH","Mensajes sin aprobación");
+
+
+define('_AM_NEWBB_DEL_ONE','Eliminar solo este mensaje');
+define('_AM_NEWBB_POSTSDELETED','El mensaje seleccionado ha sido eliminado.');
+define('_AM_NEWBB_NOAPPROVEPOST','Actualmente no hay mensajes pendientes de aprobación.');
+define('_AM_NEWBB_SUBJECTC','Asunto:');
+define('_AM_NEWBB_MESSAGEICON','Icono del mensaje:');
+define('_AM_NEWBB_MESSAGEC','Mensaje:');
+define('_AM_NEWBB_CANCELPOST','Cancelar envío');
+define('_AM_NEWBB_GOTOMOD','Ir al módulo');
+
+define('_AM_NEWBB_PREFERENCES','Preferencias del módulo');
+define('_AM_NEWBB_POLLMODULE','Modulo de encuestas de Xoops');
+define('_AM_NEWBB_POLL_OK','Listo para ser usado');
+define('_AM_NEWBB_GDLIB1','Librería GD1:');
+define('_AM_NEWBB_GDLIB2','Librería GD2:');
+define('_AM_NEWBB_AUTODETECTED','Autodetectada: ');
+define('_AM_NEWBB_AVAILABLE','Disponible');
+define('_AM_NEWBB_NOTAVAILABLE','<font color="red">No disponible</font>');
+define('_AM_NEWBB_NOTWRITABLE','<font color="red">No escribible por el servidor</font>');
+define('_AM_NEWBB_IMAGEMAGICK','ImageMagicK');
+define('_AM_NEWBB_IMAGEMAGICK_NOTSET','No ajustado');
+define('_AM_NEWBB_ATTACHPATH','Ruta para los archivos adjuntos');
+define('_AM_NEWBB_THUMBPATH','Ruta para las miniaturas');
+//define('_AM_NEWBB_RSSPATH','Ruta para el servicio RSS');
+define('_AM_NEWBB_REPORT','Mensajes reportados');
+define('_AM_NEWBB_REPORT_PENDING','Pendientes');
+define('_AM_NEWBB_REPORT_PROCESSED','Procesados');
+
+define('_AM_NEWBB_CREATETHEDIR','Crear');
+define('_AM_NEWBB_SETMPERM','Ajustar los permisos.');
+define('_AM_NEWBB_DIRCREATED','El directorio ha sido creado.');
+define('_AM_NEWBB_DIRNOTCREATED','El directorio no ha podido ser creado.');
+define('_AM_NEWBB_PERMSET','Los permisos han sido establecidos.');
+define('_AM_NEWBB_PERMNOTSET','Los permisos NO han podido ser establecidos.');
+
+define('_AM_NEWBB_DIGEST','Notificación de selecciones');
+define('_AM_NEWBB_DIGEST_PAST','<font color="red">Ha sido enviada hace %d minutos.</font>');
+define('_AM_NEWBB_DIGEST_NEXT','Será enviada en %d minutos.');
+define('_AM_NEWBB_DIGEST_ARCHIVE','Historial de selecciones');
+define('_AM_NEWBB_DIGEST_SENT','Selección procesada');
+define('_AM_NEWBB_DIGEST_FAILED','Selección NO procesada');
+
+// admin_forum_manager.php
+define("_AM_NEWBB_NAME","Nombre");
+define("_AM_NEWBB_CREATEFORUM","Crear foro");
+define("_AM_NEWBB_EDIT","Editar");
+define("_AM_NEWBB_CLEAR","Limpiar");
+define("_AM_NEWBB_DELETE","Eliminar");
+define("_AM_NEWBB_ADD","Agregar");
+define("_AM_NEWBB_MOVE","Mover");
+define("_AM_NEWBB_ORDER","Ordenar");
+define("_AM_NEWBB_TWDAFAP","Nota: Esta acción eliminará el foro con todos los mensajes que existan dentro del mismo.<br><br>ADVERTENCIA: ¿Está seguro de querer eliminar este foro?");
+define("_AM_NEWBB_FORUMREMOVED","El foro y todos sus mensajes ha sido eliminado.");
+define("_AM_NEWBB_CREATENEWFORUM","Crear un nuevo foro");
+define("_AM_NEWBB_EDITTHISFORUM","Editar un foro:");
+define("_AM_NEWBB_SET_FORUMORDER","Ajustar orden del foro:");
+define("_AM_NEWBB_ALLOWPOLLS","Permitir encuestas:");
+define("_AM_NEWBB_ATTACHMENT_SIZE" ,"Tamaño max. en kb:");
+define("_AM_NEWBB_ALLOWED_EXTENSIONS", "Permitir extensiones:<span style='font-size: xx-small; font-weight: normal; display: block;'>Extensiones separadas por |</span>");
+//define("_AM_NEWBB_ALLOW_ATTACHMENTS", "Permitir archivos adjuntos:");
+define("_AM_NEWBB_ALLOWHTML","Permitir HTML:");
+define("_AM_NEWBB_YES","Si");
+define("_AM_NEWBB_NO","No");
+define("_AM_NEWBB_ALLOWSIGNATURES","Permitir firmas:");
+define("_AM_NEWBB_HOTTOPICTHRESHOLD","Cuantos mensajes calientes mostrar (Hot Topic Threshold):");
+//define("_AM_NEWBB_POSTPERPAGE","Mensajes por página:<span style='font-size: xx-small; font-weight: normal; display: block;'>(Este es el número de mensajes<br> por tema que serán<br> mostrados en cada página)</span>");
+//define("_AM_NEWBB_TOPICPERFORUM","Temas por foro:<span style='font-size: xx-small; font-weight: normal; display: block;'>(Este es el número de temas<br> ppor foro que serán<br> mostrados en cada página)</span>");
+//define("_AM_NEWBB_SHOWNAME","Reemplazar nombre se usuario con el nombre real:");
+//define("_AM_NEWBB_SHOWICONSPANEL","Mostrar panel de iconos:");
+//define("_AM_NEWBB_SHOWSMILIESPANEL","Mostar panel de caritas:");
+define("_AM_NEWBB_MODERATOR_REMOVE","Quitar moderadores actuales");
+define("_AM_NEWBB_MODERATOR_ADD","Agregar moderadores");
+define("_AM_NEWBB_ALLOW_SUBJECT_PREFIX", "Permitir un prefijo en el asunto");
+define("_AM_NEWBB_ALLOW_SUBJECT_PREFIX_DESC", "Permite incluir un prefijo en el asunto del mensaje, para por ejemplo indicar que el problema ha sido resuelto.");
+
+
+// admin_cat_manager.php
+
+define("_AM_NEWBB_SETCATEGORYORDER","Ordenar categoría:");
+define("_AM_NEWBB_ACTIVE","Activa");
+define("_AM_NEWBB_INACTIVE","Inactiva");
+define("_AM_NEWBB_STATE","Estado:");
+define("_AM_NEWBB_CATEGORYDESC","Descripción de la categoría:");
+define("_AM_NEWBB_SHOWDESC","Mostrar descripción:");
+define("_AM_NEWBB_IMAGE","Imágen:");
+//define("_AM_NEWBB_SPONSORIMAGE","Imagen patrocinador:");
+define("_AM_NEWBB_SPONSORLINK","Enlace patrocinador:");
+define("_AM_NEWBB_DELCAT","Eliminar categoría");
+define("_AM_NEWBB_WAYSYWTDTTAL","Nota: Esta acción NO eliminará los foros que dependan de esta categría, para eliminarlos deberá hacerlo por la vía editar foros.<br><br>ADVERTENCIA: ¿Está seguro de querer eliminar esta categoría?");
+
+
+
+//%%%%%%        File Name  admin_forums.php           %%%%%
+define("_AM_NEWBB_FORUMNAME","Nombre del foro:");
+define("_AM_NEWBB_FORUMDESCRIPTION","Descripción del foro:");
+define("_AM_NEWBB_MODERATOR","Moderadores:");
+define("_AM_NEWBB_REMOVE","Eliminar");
+define("_AM_NEWBB_CATEGORY","Categoría:");
+define("_AM_NEWBB_DATABASEERROR","Error de base de datos");
+define("_AM_NEWBB_CATEGORYUPDATED","Categoría actualizada.");
+define("_AM_NEWBB_EDITCATEGORY","Editar categoría:");
+define("_AM_NEWBB_CATEGORYTITLE","Título de la categoría:");
+define("_AM_NEWBB_CATEGORYCREATED","Categoría creada.");
+define("_AM_NEWBB_CREATENEWCATEGORY","Crear una nueva categoría");
+define("_AM_NEWBB_FORUMCREATED","Foro creado.");
+define("_AM_NEWBB_ACCESSLEVEL","Nivel de acceso global:");
+define("_AM_NEWBB_CATEGORY1","Categoría");
+define("_AM_NEWBB_FORUMUPDATE","Configuración del foro actualizada");
+define("_AM_NEWBB_FORUM_ERROR","ERROR: Error en la configuración del foro.");
+define("_AM_NEWBB_CLICKBELOWSYNC","Al hacer clic en el siguiente botón sincronizará los foros y temas con los datos correctos desde la base de datos.<br /> Úselo si considera que existe una discrepancia en el orden de foros y temas.");
+define("_AM_NEWBB_SYNCHING","Sincronizando foros y temas (Esta operación puede tardar, sea paciente)");
+define("_AM_NEWBB_CATEGORYDELETED","Categoría eliminada.");
+define("_AM_NEWBB_MOVE2CAT","Mover a la categoría:");
+define("_AM_NEWBB_MAKE_SUBFORUM_OF","Crear un subforo de:");
+define("_AM_NEWBB_MSG_FORUM_MOVED","Foro movido.");
+define("_AM_NEWBB_MSG_ERR_FORUM_MOVED","Error al mover foro.");
+define("_AM_NEWBB_SELECT","< Seleccione >");
+define("_AM_NEWBB_MOVETHISFORUM","Mover este foro");
+define("_AM_NEWBB_MERGE","Combinar");
+define("_AM_NEWBB_MERGETHISFORUM","Combinar este foro");
+define("_AM_NEWBB_MERGETO_FORUM","Combinar este foro con:");
+define("_AM_NEWBB_MSG_FORUM_MERGED","El foro ha sido combinado.");
+define("_AM_NEWBB_MSG_ERR_FORUM_MERGED","Error al combinar foro.");
+
+//%%%%%%        File Name  admin_forum_reorder.php           %%%%%
+define("_AM_NEWBB_REORDERID","ID");
+define("_AM_NEWBB_REORDERTITLE","Título");
+define("_AM_NEWBB_REORDERWEIGHT","Posición");
+define("_AM_NEWBB_SETFORUMORDER","Ajustar orden del foro");
+define("_AM_NEWBB_BOARDREORDER","El foro ha sido reordenado");
+
+// admin_permission.php
+define("_AM_NEWBB_PERMISSIONS_TO_THIS_FORUM","Permisos de los temas de este foro");
+define("_AM_NEWBB_CAT_ACCESS","Acceso a Categoría");
+define("_AM_NEWBB_CAN_ACCESS","Pueden acceder");
+define("_AM_NEWBB_CAN_VIEW","Pueden ver");
+define("_AM_NEWBB_CAN_POST","Pueden enviar");
+define("_AM_NEWBB_CAN_REPLY","Pueden responder");
+define("_AM_NEWBB_CAN_EDIT","Pueden editar");
+define("_AM_NEWBB_CAN_DELETE","Pueden eliminar");
+define("_AM_NEWBB_CAN_ADDPOLL","Pueden agregar encuesta");
+define("_AM_NEWBB_CAN_VOTE","Pueden votar");
+define("_AM_NEWBB_CAN_ATTACH","Pueden adjuntar archivos");
+define("_AM_NEWBB_CAN_NOAPPROVE","Pueden enviar sin aprobación");
+define("_AM_NEWBB_ACTION","Acción");
+
+define("_AM_NEWBB_PERM_TEMPLATE","Plantilla de Permisos por defecto");
+define("_AM_NEWBB_PERM_TEMPLATE_DESC","Podrá ser aplicado a un foro ");
+define("_AM_NEWBB_PERM_FORUMS","Seleccionar foros");
+define("_AM_NEWBB_PERM_TEMPLATE_CREATED","Se ha creado la plantilla de permisos");
+define("_AM_NEWBB_PERM_TEMPLATE_ERROR","Errores ocurridos durante la creación de la plantilla de permisos ");
+define("_AM_NEWBB_PERM_TEMPLATEAPP","Aplicar permisos de");
+define("_AM_NEWBB_PERM_TEMPLATE_APPLIED","Los permisos por defecto se han aplicado a los foros");
+define("_AM_NEWBB_PERM_ACTION","Acciones de los Permisos");
+define("_AM_NEWBB_PERM_SETBYGROUP","Fijar permisos directamente al grupo");
+
+// admin_forum_prune.php
+
+define ("_AM_NEWBB_PRUNE_RESULTS_TITLE","Purgar resultados");
+define ("_AM_NEWBB_PRUNE_RESULTS_TOPICS","Temas purgados");
+define ("_AM_NEWBB_PRUNE_RESULTS_POSTS","Mensajes purgados");
+define ("_AM_NEWBB_PRUNE_RESULTS_FORUMS","Foros purgados");
+define ("_AM_NEWBB_PRUNE_STORE","Guardar mensajes en este foro en vez de eliminarlos");
+define ("_AM_NEWBB_PRUNE_ARCHIVE","Hacer una copia de los mensajes en un archivo");
+define ("_AM_NEWBB_PRUNE_FORUMSELERROR","Ha olvidado seleccionar los foros que desea purgar");
+
+define ("_AM_NEWBB_PRUNE_DAYS","Remover mensajes sin respuesta en:");
+define ("_AM_NEWBB_PRUNE_FORUMS","Foros que serán purgados");
+define ("_AM_NEWBB_PRUNE_STICKY","Conservar los temas pegados");
+define ("_AM_NEWBB_PRUNE_DIGEST","Conservar temas de la selección");
+define ("_AM_NEWBB_PRUNE_LOCK","Conservar los temas bloqueados");
+define ("_AM_NEWBB_PRUNE_HOT","Conservar temas con más de estas respuestas");
+define ("_AM_NEWBB_PRUNE_SUBMIT","Aceptar");
+define ("_AM_NEWBB_PRUNE_RESET","Reanudar");
+define ("_AM_NEWBB_PRUNE_YES","Si");
+define ("_AM_NEWBB_PRUNE_NO","No");
+define ("_AM_NEWBB_PRUNE_WEEK","1 semana");
+define ("_AM_NEWBB_PRUNE_2WEEKS","2 semanas");
+define ("_AM_NEWBB_PRUNE_MONTH","1 mes");
+define ("_AM_NEWBB_PRUNE_2MONTH","2 meses");
+define ("_AM_NEWBB_PRUNE_4MONTH","4 meses");
+define ("_AM_NEWBB_PRUNE_YEAR","1 año");
+define ("_AM_NEWBB_PRUNE_2YEARS","2 años");
+
+// About.php constants
+define('_AM_NEWBB_AUTHOR_INFO', "Información del autor");
+define('_AM_NEWBB_AUTHOR_NAME', "Autor");
+define('_AM_NEWBB_AUTHOR_WEBSITE', "Sitio Web del autor");
+define('_AM_NEWBB_AUTHOR_EMAIL', "Email del autor");
+define('_AM_NEWBB_AUTHOR_CREDITS', "Créditos");
+define('_AM_NEWBB_MODULE_INFO', "Información sobre el desarrollo del módulo");
+define('_AM_NEWBB_MODULE_STATUS', "Estado del módulo");
+define('_AM_NEWBB_MODULE_DEMO', "Sitio de demostración");
+define('_AM_NEWBB_MODULE_SUPPORT', "Sitio oficial de soporte");
+define('_AM_NEWBB_MODULE_BUG', "Reportar un bug en este módulo");
+define('_AM_NEWBB_MODULE_FEATURE', "Sugerir una nueva característica para este módulo");
+define('_AM_NEWBB_MODULE_DISCLAIMER', "Aviso legal y descargo de responsabilidades");
+define('_AM_NEWBB_AUTHOR_WORD', "Saludos y agradecimientos");
+define('_AM_NEWBB_BY','Por');
+define('_AM_NEWBB_AUTHOR_WORD_EXTRA', "
+");
+
+// admin_report.php
+define("_AM_NEWBB_REPORTADMIN","Gestor de reportes");
+define("_AM_NEWBB_PROCESSEDREPORT","Ver reportes procesados");
+define("_AM_NEWBB_PROCESSREPORT","Reportes procesados");
+define("_AM_NEWBB_REPORTTITLE","Título del reporte");
+define("_AM_NEWBB_REPORTEXTRA","Extra");
+define("_AM_NEWBB_REPORTPOST","Mensaje reportado");
+define("_AM_NEWBB_REPORTTEXT","Texto reportado");
+define("_AM_NEWBB_REPORTMEMO","Memoria de reportes");
+
+// admin_report.php
+define("_AM_NEWBB_DIGESTADMIN","Gestor de selecciones");
+define("_AM_NEWBB_DIGESTCONTENT","Contenido de las selecciones");
+
+// admin_votedata.php
+define("_AM_NEWBB_VOTE_RATINGINFOMATION", "Información de votos");
+define("_AM_NEWBB_VOTE_TOTALVOTES", "Votos en total: ");
+define("_AM_NEWBB_VOTE_REGUSERVOTES", "Votos de usuarios registrados: %s");
+define("_AM_NEWBB_VOTE_ANONUSERVOTES", "Votos de usuarios anónimos: %s");
+define("_AM_NEWBB_VOTE_USER", "Usuario");
+define("_AM_NEWBB_VOTE_IP", "Dirección IP");
+define("_AM_NEWBB_VOTE_USERAVG", "Porcentaje de votaciones");
+define("_AM_NEWBB_VOTE_TOTALRATE", "Total valoraciones");
+define("_AM_NEWBB_VOTE_DATE", "Fecha");
+define("_AM_NEWBB_VOTE_RATING", "Valoración");
+define("_AM_NEWBB_VOTE_NOREGVOTES", "Sin votos de usuarios registrados");
+define("_AM_NEWBB_VOTE_NOUNREGVOTES", "Sin votos de usuarios anónimos");
+define("_AM_NEWBB_VOTEDELETED", "Información de votación eliminada.");
+define("_AM_NEWBB_VOTE_ID", "ID");
+define("_AM_NEWBB_VOTE_FILETITLE", "Título del hilo");
+define("_AM_NEWBB_VOTE_DISPLAYVOTES", "Información de las votaciones");
+define("_AM_NEWBB_VOTE_NOVOTES", "Sin votos que mostrar");
+define("_AM_NEWBB_VOTE_DELETE", "Sin votos que mostrar");
+define("_AM_NEWBB_VOTE_DELETEDSC", "<b>Elimina</b> de la base de datos la información sobre las votaciones seleccionadas.");
+?>
