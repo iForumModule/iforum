7484488e7c9d12cb2bbc7bad649d6dc8d0b3bb27
Ronald <pietjebell31@hotmail.com>
2008-11-20 18:26:21 +0000

    Modified admin menu using displayAdminMenu()
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/iforum/trunk@7082 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- admin/menu.php
+++ admin/menu.php
@@ -1,66 +1,83 @@
 <?php 
-// $Id: menu.php,v 1.3 2005/10/19 17:20:32 phppp Exp $
-// ------------------------------------------------------------------------ //
-// XOOPS - PHP Content Management System                      //
-// Copyright (c) 2000 XOOPS.org                           //
-// <http://www.xoops.org/>                             //
-// ------------------------------------------------------------------------ //
-// This program is free software; you can redistribute it and/or modify     //
-// it under the terms of the GNU General Public License as published by     //
-// the Free Software Foundation; either version 2 of the License, or        //
-// (at your option) any later version.                                      //
-// //
-// You may not change or alter any portion of this comment or credits       //
-// of supporting developers from this source code or any supporting         //
-// source code which is considered copyrighted (c) material of the          //
-// original comment or credit authors.                                      //
-// //
-// This program is distributed in the hope that it will be useful,          //
-// but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-// GNU General Public License for more details.                             //
-// //
-// You should have received a copy of the GNU General Public License        //
-// along with this program; if not, write to the Free Software              //
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-// ------------------------------------------------------------------------ //
-// Author: Kazumi Ono (AKA onokazu)                                          //
-// URL: http://www.myweb.ne.jp/, http://www.xoops.org/, http://jp.xoops.org/ //
-// Project: The XOOPS Project                                                //
-// ------------------------------------------------------------------------- //
+/**
+* iForum - a forum module for ImpressCMS
+*
+* Based upon NewBB/CBB
+*
+* File: menu.php
+*
+* @copyright		http://www.xoops.org/ The XOOPS Project
+* @copyright		XOOPS_copyrights.txt
+* @copyright		http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* @package		iForum 
+* @since			1.0
+* @author		McDonald
+* @version		$Id$
+*/
+
+$admin_dirname = basename( dirname( dirname( __FILE__ ) ) );
+
+global $xoopsModule, $xoopsConfig;
 
 $adminmenu[0]['title']  = _MI_NEWBB_ADMENU_INDEX;
-$adminmenu[0]['link']   = "admin/index.php";
+$adminmenu[0]['link']   = 'admin/index.php';
 $adminmenu[0]['icon']   = 'images/home.png';
 $adminmenu[1]['title']  = _MI_NEWBB_ADMENU_CATEGORY;
-$adminmenu[1]['link']   = "admin/admin_cat_manager.php?op=manage";
+$adminmenu[1]['link']   = 'admin/admin_cat_manager.php?op=manage';
 $adminmenu[1]['icon']   = 'images/folder.png';
 $adminmenu[2]['title']  = _MI_NEWBB_ADMENU_FORUM;
-$adminmenu[2]['link']   = "admin/admin_forum_manager.php?op=manage";
+$adminmenu[2]['link']   = 'admin/admin_forum_manager.php?op=manage';
 $adminmenu[2]['icon']   = 'images/imforum_iconbig.png';
 $adminmenu[3]['title']  = _MI_NEWBB_ADMENU_PERMISSION;
-$adminmenu[3]['link']   = "admin/admin_permissions.php";
+$adminmenu[3]['link']   = 'admin/admin_permissions.php';
 $adminmenu[3]['icon']   = 'images/permission.png';
 $adminmenu[4]['title']  = _MI_NEWBB_ADMENU_BLOCK;
-$adminmenu[4]['link']   = "admin/admin_blocks.php";
+$adminmenu[4]['link']   = 'admin/admin_blocks.php';
 $adminmenu[4]['icon']   = 'images/blocks.png';
 $adminmenu[5]['title']  = _MI_NEWBB_ADMENU_SYNC;
-$adminmenu[5]['link']   = "admin/admin_forum_manager.php?op=sync";
+$adminmenu[5]['link']   = 'admin/admin_forum_manager.php?op=sync';
 $adminmenu[5]['icon']   = 'images/sync.png';
 $adminmenu[6]['title']  = _MI_NEWBB_ADMENU_ORDER;
-$adminmenu[6]['link']   = "admin/admin_forum_reorder.php";
+$adminmenu[6]['link']   = 'admin/admin_forum_reorder.php';
 $adminmenu[6]['icon']   = 'images/order.png';
 $adminmenu[7]['title']  = _MI_NEWBB_ADMENU_PRUNE;
-$adminmenu[7]['link']   = "admin/admin_forum_prune.php";
+$adminmenu[7]['link']   = 'admin/admin_forum_prune.php';
 $adminmenu[7]['icon']   = 'images/prune.png';
 $adminmenu[8]['title']  = _MI_NEWBB_ADMENU_REPORT;
-$adminmenu[8]['link']   = "admin/admin_report.php";
+$adminmenu[8]['link']   = 'admin/admin_report.php';
 $adminmenu[8]['icon']   = 'images/reports.png';
 $adminmenu[9]['title']  = _MI_NEWBB_ADMENU_DIGEST;
-$adminmenu[9]['link']   = "admin/admin_digest.php";
+$adminmenu[9]['link']   = 'admin/admin_digest.php';
 $adminmenu[9]['icon']   = 'images/digest.png';
 $adminmenu[10]['title'] = _MI_NEWBB_ADMENU_VOTE;
-$adminmenu[10]['link']  = "admin/admin_votedata.php";
+$adminmenu[10]['link']  = 'admin/admin_votedata.php';
 $adminmenu[10]['icon']  = 'images/votes.png';
+
+if ( isset( $xoopsModule ) ) {
+	
+	if ( file_exists( ICMS_ROOT_PATH . '/modules/' . $admin_dirname . '/language/' . $xoopsConfig['language'] . '/admin.php' ) ) {
+		include_once ICMS_ROOT_PATH . '/modules/' . $admin_dirname . '/language/' . $xoopsConfig['language'] . '/admin.php';
+	} else {
+		include_once ICMS_ROOT_PATH . '/modules/' . $admin_dirname . '/language/english/admin.php';
+	}
+		
+	$i = -1;
+	
+	$i++;
+	$headermenu[$i]['title'] = $xoopsModule -> getVar( 'name' );
+	$headermenu[$i]['link']  = ICMS_URL . '/modules/' . $admin_dirname;
+	
+	$i++;
+	$headermenu[$i]['title'] = _PREFERENCES;
+	$headermenu[$i]['link']  = '../../system/admin.php?fct=preferences&amp;op=showmod&amp;mod=' . $xoopsModule -> getVar( 'mid' );
+	
+	$i++;
+	$headermenu[$i]['title'] = _AM_NEWBB_ABOUT;
+	$headermenu[$i]['link']  = ICMS_URL . '/modules/' . $admin_dirname . '/admin/about.php';
+	
+}
 
 ?>
--- class/art/functions.admin.php
+++ class/art/functions.admin.php
@@ -5,83 +5,15 @@
  * @copyright	The XOOPS project http://www.xoops.org/
  * @license		http://www.fsf.org/copyleft/gpl.html GNU public license
  * @author		Taiwen Jiang (phppp or D.J.) <php_pp@hotmail.com>
+ * @author		McDonald <pietjebell31@hotmail.com>
  * @since		1.00
  * @version		$Id$
  * @package		Frameworks::art
  */
 
-if(!defined("FRAMEWORKS_ART_FUNCTIONS_ADMIN")):
-define("FRAMEWORKS_ART_FUNCTIONS_ADMIN", true);
-
-defined("FRAMEWORKS_ART_FUNCTIONS_INI") || include_once dirname(__FILE__)."/functions.ini.php";
-
-function loadModuleAdminMenu ($currentoption = -1, $breadcrumb = "")
-{
-	// For XOOPS 2.2*
-	if(isset($GLOBALS["xTheme"]) && method_exists($GLOBALS["xTheme"], "loadModuleAdminMenu")){
-		$GLOBALS["xTheme"]->loadModuleAdminMenu($currentoption, $breadcrumb);
-	// For XOOPS 2.0*, 2.3*
-	}else{
-		echo _loadModuleAdminMenu($currentoption, $breadcrumb);
-	}
-	return true;
+function loadModuleAdminMenu ($currentoption = 0, $breadcrumb = '')  {
+	global $xoopsModule;
+	$xoopsModule -> displayAdminMenu( $currentoption, $xoopsModule -> name() . ' | ' . $breadcrumb );
 }
 
-function _loadModuleAdminMenu($currentoption, $breadcrumb = "")
-{
-	$adminmenu = array();
-	
-	if(!@include XOOPS_ROOT_PATH."/modules/".$GLOBALS["xoopsModule"]->getVar("dirname")."/".$GLOBALS["xoopsModule"]->getInfo("adminmenu")){
-		return null;
-	}
-	$breadcrumb = empty($breadcrumb) ? $adminmenu[$currentoption]["title"] : $breadcrumb;
-	$module_link = XOOPS_URL."/modules/".$GLOBALS["xoopsModule"]->getVar("dirname")."/";
-	$image_link = XOOPS_URL."/modules/".basename( dirname(  dirname(  dirname( __FILE__ ) ) ) )."/images";
-	
-	$adminmenu_text ='
-	<style type="text/css">
-	<!--
-	#buttontop { float:'.(( defined("_ADM_USE_RTL") && _ADM_USE_RTL )?'right':'left').'; width:100%; background: #e7e7e7; font-size:93%; line-height:normal; border-top: 1px solid black; border-left: 1px solid black; border-right: 1px solid black; margin: 0;}
-	#buttonbar { float:'.(( defined("_ADM_USE_RTL") && _ADM_USE_RTL )?'right':'left').'; width:100%; background: #e7e7e7 url("'.$image_link.'/modadminbg.gif") repeat-x left bottom; font-size:93%; line-height:normal; border-left: 1px solid black; border-right: 1px solid black; margin-bottom: 12px;}
-	#buttonbar ul { margin:0; margin-top: 15px; padding:10px 10px 0; list-style:none; }
-	#buttonbar li { display:inline; margin:0; padding:0; }
-	#buttonbar a { float:'.(( defined("_ADM_USE_RTL") && _ADM_USE_RTL )?'right':'left').'; background:url("'.$image_link.'/left_both.gif") no-repeat left top; margin:0; padding:0 0 0 9px; border-bottom:1px solid #000; text-decoration:none; }
-	#buttonbar a span { float:'.(( defined("_ADM_USE_RTL") && _ADM_USE_RTL )?'right':'left').'; display:block; background:url("'.$image_link.'/right_both.gif") no-repeat right top; padding:5px 15px 4px 6px; font-weight:bold; color:#765; }
-	/* Commented Backslash Hack hides rule from IE5-Mac \*/
-	#buttonbar a span {float:none;}
-	/* End IE5-Mac hack */
-	#buttonbar a:hover span { color:#333; }
-	#buttonbar .current a { background-position:0 -150px; border-width:0; }
-	#buttonbar .current a span { background-position:100% -150px; padding-bottom:5px; color:#333; }
-	#buttonbar a:hover { background-position:0% -150px; }
-	#buttonbar a:hover span { background-position:100% -150px; }	
-	//-->
-	</style>
-	<div id="buttontop">
-	 <table style="width: 100%; padding: 0; " cellspacing="0">
-	     <tr>
-	         <td style="width: 70%; font-size: 10px; text-align: '.(( defined("_ADM_USE_RTL") && _ADM_USE_RTL )?'right':'left').'; color: #2F5376; padding: 0 6px; line-height: 18px;">
-	             <a href="../index.php">'.$GLOBALS["xoopsModule"]->getVar("name").'</a>
-	         </td>
-	         <td style="width: 30%; font-size: 10px; text-align: '.(( defined("_ADM_USE_RTL") && _ADM_USE_RTL )?'left':'right').'; color: #2F5376; padding: 0 6px; line-height: 18px;">
-	             <b>'.$GLOBALS["xoopsModule"]->getVar("name").'</b>&nbsp;'.$breadcrumb.'
-	         </td>
-	     </tr>
-	 </table>
-	</div>
-	<div id="buttonbar">
-	 <ul>
-	';
-	foreach(array_keys($adminmenu) as $key){
-		$adminmenu_text .= (($currentoption == $key) ? '<li class="current">':'<li>').'<a href="'.$module_link.$adminmenu[$key]["link"].'"><span>'.$adminmenu[$key]["title"].'</span></a></li>';
-	}
-	$adminmenu_text .= '<li><a href="'.XOOPS_URL.'/modules/system/admin.php?fct=preferences&op=showmod&mod='.$GLOBALS["xoopsModule"]->getVar("mid").'"><span>'._PREFERENCES.'</span></a></li>';
-	$adminmenu_text .='
-	 </ul>
-	</div>
-	<br style="clear:both;" />';
-	
-	return $adminmenu_text;
-}
-endif;
 ?>
