bbf36bd158e642676140898b89899b0ea888eb25
David <david.j@impresscms.org>
2025-02-13 12:08:47 +0100

    return of the eluse getAllPosts

--- src/class/topic.php
+++ src/class/topic.php
@@ -222,12 +222,12 @@ class IforumTopicHandler extends ArtObjectHandler {
 		return $post_id;
 	}
 
-	/*function &getAllPosts(&$topic, $order = "ASC", $perpage = 10, &$start, $post_id = 0, $type = "")
+	function &getAllPosts(&$topic, $order = "ASC", $perpage = 10, &$start, $post_id = 0, $type = "")
 	{
 		$ret = array();
-		$perpage = (intval($perpage) > 0) ? intval($perpage) :
+		$perpage = ((int)$perpage > 0) ? (int)$perpage :
 		 (empty(icms::$module->config['posts_per_page']) ? 10 : icms::$module->config['posts_per_page']);
-		$start = intval($start);
+		$start = (int)$start;
 		switch($type)
 		{
 			case "pending":
@@ -253,7 +253,7 @@ class IforumTopicHandler extends ArtObjectHandler {
 				$operator_for_position = '<' ;
 			}
 			//$approve_criteria = ' AND approved = 1'; // any others?
-			$sql = "SELECT COUNT(*) FROM " . $this->db->prefix('bb_posts') . " AS p WHERE p.topic_id=" . intval($topic->getVar('topic_id')) . $approve_criteria . " AND p.post_id $operator_for_position $post_id";
+			$sql = "SELECT COUNT(*) FROM " . $this->db->prefix('bb_posts') . " AS p WHERE p.topic_id=" . (int)$topic->getVar('topic_id') . $approve_criteria . " AND p.post_id $operator_for_position $post_id";
 			$result = $this->db->query($sql);
 			if (!$result)
 			{
@@ -261,7 +261,7 @@ class IforumTopicHandler extends ArtObjectHandler {
 				return $ret;
 			}
 			list($position) = $this->db->fetchRow($result);
-			$start = intval($position / $perpage) * $perpage;
+			$start = (int)($position / $perpage) * $perpage;
 		}
 
 		$sql = 'SELECT p.*, t.* FROM ' . $this->db->prefix('bb_posts') . ' p, ' . $this->db->prefix('bb_posts_text') . " t WHERE p.topic_id=" . $topic->getVar('topic_id') . " AND p.post_id = t.post_id" . $approve_criteria . " ORDER BY p.post_id $order";
@@ -280,7 +280,7 @@ class IforumTopicHandler extends ArtObjectHandler {
 			unset($post);
 		}
 		return $ret;
-	}*/
+	}
 
 	function &getPostTree(&$postArray, $pid = 0)
 	{
